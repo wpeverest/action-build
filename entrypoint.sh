d1f5462f62b356771ac0363c0134e1cf4b3e1756
asheshmagar <ashesh.ido@gmail.com>
2026-01-12 09:26:02 +0545

    chore: update build cmd

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ echo "Installing PHP and JS dependencies..."
 $INSTALL_CMD
 composer install || exit "$?"
 echo "Running JS Build..."
-$RUN_CMD build:core || exit "$?"
+$RUN_CMD build || exit "$?"
 echo "Cleaning up PHP dependencies..."
 composer install --no-dev || exit "$?"
 
